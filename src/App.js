4cfef3190d6150df188d3f8f2f55d1c93904c523
leehyeeun <leehe0430@gmail.com>
2022-03-18 11:40:56 +0900

    react_velopert ch14 newViewer UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./App.css";
+import NewsList from "./components/NewsList";
 
 function App() {
-  return <div className="App"></div>;
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=aea770ed6949490493b34741a14f63bb"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div className="App">
+      <NewsList />
+    </div>
+  );
 }
 
 export default App;
